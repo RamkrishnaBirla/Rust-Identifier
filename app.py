4c82c1fc4eabdf7a59f8a0fc5add5016270a5049
unknown <ramkbirla1048@gmail.com>
2025-11-08 22:29:33 +0530

    fix

--- app.py
+++ app.py
@@ -65,4 +65,6 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))  # default for local, Render uses its own
+    app.run(host='0.0.0.0', port=port)
